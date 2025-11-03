6ebae213e68745c7188350f9557b7e20d67397d8
Denis Holdeew <hold.denis@gmail.com>
2025-11-03 11:04:33 +0100

    Setup word validtor

--- src/main/java/com/dehold/contentmanager/validation/step/ForbiddenWordValidator.java
+++ src/main/java/com/dehold/contentmanager/validation/step/ForbiddenWordValidator.java
@@ -2,17 +2,23 @@ package com.dehold.contentmanager.validation.step;
 
 import com.dehold.contentmanager.content.Content;
 import com.dehold.contentmanager.validation.model.ValidationResult;
+import com.dehold.contentmanager.validation.service.ForbiddenWordsService;
+import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 import java.util.function.Function;
 
+@Component
 public class ForbiddenWordValidator<T extends Content> implements ValidationStep<T> {
 
+    private final ForbiddenWordsService service;
     private final Function<T, String> getter;
     private final String fieldName;
     private final UUID userId;
 
-    public ForbiddenWordValidator(Function<T, String> getter, String fieldName, UUID userId) {
+    public ForbiddenWordValidator(ForbiddenWordsService service, Function<T, String> getter, String fieldName,
+                                  UUID userId) {
+        this.service = service;
         this.getter = getter;
         this.fieldName = fieldName;
         this.userId = userId;
