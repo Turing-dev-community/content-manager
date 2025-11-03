8bb01ec86218b972d7fd0a1e94cf29a772d8e4b5
Denis Holdeew <hold.denis@gmail.com>
2025-11-03 11:14:20 +0100

    Add test

---
+++ src/test/java/com/dehold/contentmanager/validation/step/ForbiddenWordValidatorTest.java
@@ -0,0 +1,53 @@
+package com.dehold.contentmanager.validation.step;
+
+import com.dehold.contentmanager.content.blogpost.model.BlogPost;
+import com.dehold.contentmanager.validation.model.ForbiddenWords;
+import com.dehold.contentmanager.validation.model.ValidationResult;
+import com.dehold.contentmanager.validation.service.ForbiddenWordsService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+class ForbiddenWordValidatorTest {
+
+    @Mock
+    ForbiddenWordsService forbiddenWordsService;
+
+    @InjectMocks
+    ForbiddenWordValidator<BlogPost> cut;
+
+    ForbiddenWords defaultForbiddenWords = new ForbiddenWords(null, null, "Default Forbidden Words", "any", "any",
+            new java.util.LinkedHashSet<>(java.util.Arrays.asList("badword1", "badword2")));
+    ForbiddenWords customForbiddenWords = new ForbiddenWords(
+            UUID.randomUUID(),
+            UUID.randomUUID(),
+            "Custom Forbidden Words",
+            "blogpost",
+            "content",
+            new java.util.LinkedHashSet<>(java.util.Arrays.asList("custombadword1", "custombadword2"))
+    );
+
+    @BeforeEach
+    void setup() {
+        List<ForbiddenWords> forbiddenWordsList = List.of(defaultForbiddenWords, customForbiddenWords);
+        when(forbiddenWordsService.findByUserId(any(UUID.class))).thenReturn(forbiddenWordsList);
+    }
+
+    @Test
+    void givenBlogPostWithNoForbiddenWords_whenValidate_thenReturnValidResult() {
+        BlogPost blogPost = new BlogPost(UUID.randomUUID(),"Some Title", "This is a clean blog post content.",
+                Instant.now(), Instant.now(), UUID.randomUUID());
+        ValidationResult result = cut.validate(blogPost);
+        assertTrue(result.isValid());
+    }
+
+}
