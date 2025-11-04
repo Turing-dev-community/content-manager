61b2575577f0e69113daa5ad8363d05d3cb2e356
Denis Holdeew <hold.denis@gmail.com>
2025-11-04 12:00:52 +0100

    ValidationPipeline model fields

--- src/main/java/com/dehold/contentmanager/validation/model/ValidationPipelineModel.java
+++ src/main/java/com/dehold/contentmanager/validation/model/ValidationPipelineModel.java
@@ -1,5 +1,16 @@
 package com.dehold.contentmanager.validation.model;
 
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
 public class ValidationPipelineModel {
+    private UUID id;
+    private UUID userId;
+    private String contentType;
+    private boolean isActive;
+    private List<ValidationStepModel> steps;
+    private Instant createdAt;
 
 }
+
