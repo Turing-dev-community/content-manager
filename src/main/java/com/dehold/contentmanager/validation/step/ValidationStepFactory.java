6be7f6fd76425e511ae1a4970e43efce6a3476ac
Denis Holdeew <hold.denis@gmail.com>
2025-11-04 08:44:32 +0100

    Unify validationStep creation

--- src/main/java/com/dehold/contentmanager/validation/step/ValidationStepFactory.java
+++ src/main/java/com/dehold/contentmanager/validation/step/ValidationStepFactory.java
@@ -1,9 +1,11 @@
 package com.dehold.contentmanager.validation.step;
 
 import com.dehold.contentmanager.content.Content;
+import com.dehold.contentmanager.validation.model.ValidationStepType;
 import com.dehold.contentmanager.validation.service.ForbiddenWordsService;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
 import java.util.UUID;
 import java.util.function.Function;
 
@@ -14,6 +16,22 @@ public class ValidationStepFactory {
 
     public ValidationStepFactory(ForbiddenWordsService forbiddenWordsService) {
         this.forbiddenWordsService = forbiddenWordsService;
+    }
+
+    public <T extends Content> ValidationStep<T> createValidationStep(ValidationStepType type,
+                                                                     Function<T, String> fieldExtractor, Map<String,
+                    String> params, String fieldName, UUID userId) {
+        return switch (type) {
+            case FORBIDDEN_WORD_VALIDATION ->
+                    createForbiddenWordValidator(fieldExtractor, fieldName, userId);
+            case LENGTH_VALIDATION -> {
+                int minLength = Integer.parseInt(params.get("minLength"));
+                int maxLength = Integer.parseInt(params.get("maxLength"));
+                yield createLengthValidator(fieldExtractor, fieldName, minLength, maxLength);
+            }
+            case PHONE_NUMBER_FORBIDDEN_VALIDATION ->
+                    createPhoneNumberValidator(fieldExtractor, fieldName);
+        };
     }
 
     public <T extends Content> ForbiddenWordValidator<T> createForbiddenWordValidator(
