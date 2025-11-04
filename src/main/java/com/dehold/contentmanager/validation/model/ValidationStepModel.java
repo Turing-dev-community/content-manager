753a849e6bd0c5ee44928d9b481e99b40fdb0fd8
Denis Holdeew <hold.denis@gmail.com>
2025-11-04 12:02:59 +0100

    ValidationStepModel fields

--- src/main/java/com/dehold/contentmanager/validation/model/ValidationStepModel.java
+++ src/main/java/com/dehold/contentmanager/validation/model/ValidationStepModel.java
@@ -1,6 +1,14 @@
 package com.dehold.contentmanager.validation.model;
 
-public class ValidationStepModel {
-    ValidationStepType validationStepType;
+import java.util.Map;
+import java.util.UUID;
 
+public class ValidationStepModel {
+    private UUID id;
+    private UUID pipelineId;
+    private ValidationStepType stepType;
+    private String fieldName;
+    private int stepOrder;
+    private Map<String, String> parameters;
+    private boolean isEnabled;
 }
