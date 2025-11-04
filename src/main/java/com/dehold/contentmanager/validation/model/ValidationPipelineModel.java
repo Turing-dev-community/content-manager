5ccf913f35f94acb39563cd3fa27972b4ac249be
Denis Holdeew <hold.denis@gmail.com>
2025-11-04 12:07:42 +0100

    Pipeline constructor + getters and stters

--- src/main/java/com/dehold/contentmanager/validation/model/ValidationPipelineModel.java
+++ src/main/java/com/dehold/contentmanager/validation/model/ValidationPipelineModel.java
@@ -12,5 +12,57 @@ public class ValidationPipelineModel {
     private List<ValidationStepModel> steps;
     private Instant createdAt;
 
+    public ValidationPipelineModel(UUID id, UUID userId, String contentType, boolean isActive, List<ValidationStepModel> steps, Instant createdAt) {
+        this.id = id;
+        this.userId = userId;
+        this.contentType = contentType;
+        this.isActive = isActive;
+        this.steps = steps;
+        this.createdAt = createdAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public List<ValidationStepModel> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<ValidationStepModel> steps) {
+        this.steps = steps;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
 }
 
