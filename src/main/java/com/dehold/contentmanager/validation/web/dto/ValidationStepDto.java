e2b2b53d8b6b4e65c5205a87c7cb52c74b1a7a25
Denis Holdeew <hold.denis@gmail.com>
2025-11-05 11:13:51 +0100

    Add id to ValidationStepDto for Updte mapping

--- src/main/java/com/dehold/contentmanager/validation/web/dto/ValidationStepDto.java
+++ src/main/java/com/dehold/contentmanager/validation/web/dto/ValidationStepDto.java
@@ -6,17 +6,27 @@ import java.util.Map;
 import java.util.UUID;
 
 public class ValidationStepDto {
+    private UUID id;
     private ValidationStepType stepType;
     private String fieldName;
     private Map<String, String> parameters;
     private boolean isEnabled;
 
-    public ValidationStepDto(ValidationStepType stepType, String fieldName, Map<String, String> parameters,
+    public ValidationStepDto(UUID id, ValidationStepType stepType, String fieldName, Map<String, String> parameters,
                              boolean isEnabled) {
+        this.id = id;
         this.stepType = stepType;
         this.fieldName = fieldName;
         this.parameters = parameters;
         this.isEnabled = isEnabled;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
     }
 
     public ValidationStepType getStepType() {
