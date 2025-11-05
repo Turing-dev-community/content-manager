34630302ffcca6470523c491323ddc4ea2ef7b2b
Denis Holdeew <hold.denis@gmail.com>
2025-11-05 12:58:28 +0100

    Add test for update

--- src/test/java/com/dehold/contentmanager/validation/web/ValidationPipelineControllerTest.java
+++ src/test/java/com/dehold/contentmanager/validation/web/ValidationPipelineControllerTest.java
@@ -3,12 +3,17 @@ package com.dehold.contentmanager.validation.web;
 import com.dehold.contentmanager.validation.model.ValidationPipelineModel;
 import com.dehold.contentmanager.validation.model.ValidationStepType;
 import com.dehold.contentmanager.validation.web.dto.ValidationPipelineCreateDto;
+import com.dehold.contentmanager.validation.web.dto.ValidationPipelineUpdateDto;
 import com.dehold.contentmanager.validation.web.dto.ValidationStepDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Map;
@@ -46,6 +51,47 @@ class ValidationPipelineControllerTest {
         assertEquals(requestDto.getContentType(), createdPipeline.getContentType());
         assertEquals(1, createdPipeline.getSteps().size());
         assertEquals(ValidationStepType.LENGTH_VALIDATION, createdPipeline.getSteps().getFirst().getStepType());
+    }
+
+    @Test
+    void givenUpdateRequest_whenValidationPipelineExists_thenUpdatesPipeline() {
+        var createRequestDto = new ValidationPipelineCreateDto();
+        createRequestDto.setUserId(UUID.randomUUID());
+        createRequestDto.setContentType("BlogPost");
+        createRequestDto.setSteps(List.of(
+                new ValidationStepDto(null, ValidationStepType.LENGTH_VALIDATION, "title", Map.of("minLength", "10",
+                        "maxLength", "500"), true)
+        ));
+
+        var createResponse = restTemplate.postForEntity("http://localhost:" + port + "/api/validation-pipeline",
+                createRequestDto, ValidationPipelineModel.class);
+        var createdPipeline = createResponse.getBody();
+        assertEquals(201, createResponse.getStatusCode().value());
+        assertNotNull(createdPipeline);
+
+        var updateRequestDto = new ValidationPipelineUpdateDto();
+        updateRequestDto.setId(createdPipeline.getId());
+        updateRequestDto.setUserId(createdPipeline.getUserId());
+        updateRequestDto.setContentType(createdPipeline.getContentType());
+        UUID validationStepId = createdPipeline.getSteps().getFirst().getId();
+        updateRequestDto.setSteps(List.of(
+                new ValidationStepDto(validationStepId, ValidationStepType.LENGTH_VALIDATION, "content", Map.of("minLength", "50",
+                        "maxLength", "2000"), true)
+        ));
+
+        HttpEntity<ValidationPipelineUpdateDto> requestEntity = new HttpEntity<>(updateRequestDto);
+        ResponseEntity<ValidationPipelineModel> response =
+                restTemplate.exchange("http://localhost:" + port + "/api/validation-pipeline/" +
+                        createdPipeline.getId(), HttpMethod.PUT,
+                        requestEntity, ValidationPipelineModel.class);
+
+        assertEquals(200, response.getStatusCode().value());
+        var updatedPipeline = response.getBody();
+        assertNotNull(updatedPipeline);
+        assertEquals(updatedPipeline.getId(), createdPipeline.getId());
+        assertEquals(1, updatedPipeline.getSteps().size());
+        assertEquals(validationStepId, updatedPipeline.getSteps().getFirst().getId());
+        assertEquals("content", updatedPipeline.getSteps().getFirst().getFieldName());
     }
 
 }
