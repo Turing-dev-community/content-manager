7e4da94d274d1dcfe6dcba980dcdd301a5dee871
Denis Holdeew <hold.denis@gmail.com>
2025-11-05 12:16:12 +0100

    Add controller

---
+++ src/main/java/com/dehold/contentmanager/validation/web/ValidationPipelineController.java
@@ -0,0 +1,10 @@
+package com.dehold.contentmanager.validation.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/validation-pipeline")
+public class ValidationPipelineController {
+}
