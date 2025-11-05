d2e5cca1bda3998f54c1db73a02951cca9820afa
Denis Holdeew <hold.denis@gmail.com>
2025-11-05 12:34:33 +0100

    Implement POST create

--- src/main/java/com/dehold/contentmanager/validation/web/ValidationPipelineController.java
+++ src/main/java/com/dehold/contentmanager/validation/web/ValidationPipelineController.java
@@ -1,10 +1,33 @@
 package com.dehold.contentmanager.validation.web;
 
 
+import com.dehold.contentmanager.validation.model.ValidationPipelineModel;
+import com.dehold.contentmanager.validation.service.ValidationPipelineService;
+import com.dehold.contentmanager.validation.web.dto.ValidationPipelineCreateDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/validation-pipeline")
 public class ValidationPipelineController {
+
+    @Autowired
+    private final ValidationPipelineService validationPipelineService;
+
+    public ValidationPipelineController(ValidationPipelineService validationPipelineService) {
+        this.validationPipelineService = validationPipelineService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<ValidationPipelineModel> createValidationPipeline(@RequestBody ValidationPipelineCreateDto dto) {
+        ValidationPipelineModel model = validationPipelineService.create(dto.toModel());
+        return new ResponseEntity<>(model, HttpStatus.CREATED);
+    }
+
 }
