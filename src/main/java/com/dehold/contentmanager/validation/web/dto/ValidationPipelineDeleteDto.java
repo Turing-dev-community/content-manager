8e3182cab52e7d1dbe83829dc4b07878cdbab3de
Denis Holdeew <hold.denis@gmail.com>
2025-11-05 11:21:33 +0100

    Deletion dto

---
+++ src/main/java/com/dehold/contentmanager/validation/web/dto/ValidationPipelineDeleteDto.java
@@ -0,0 +1,15 @@
+package com.dehold.contentmanager.validation.web.dto;
+
+import java.util.UUID;
+
+public class ValidationPipelineDeleteDto {
+    private UUID id;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
