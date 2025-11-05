1e58bd504b43f597f2385777ef11cd1e2dc79297
Denis Holdeew <hold.denis@gmail.com>
2025-11-05 17:03:32 +0100

    Adapt test

--- src/test/java/com/dehold/contentmanager/validation/web/ValidationPipelineControllerTest.java
+++ src/test/java/com/dehold/contentmanager/validation/web/ValidationPipelineControllerTest.java
@@ -167,13 +167,14 @@ class ValidationPipelineControllerTest {
 
         String url = String.format("http://localhost:%d/api/validation-pipelines?userId=%s&contentType=%s",
                 port, userId, contentType);
-        ResponseEntity<ValidationPipelineModel> getResponse = restTemplate.getForEntity(
+        ResponseEntity<ValidationPipelineModel[]> getResponse = restTemplate.getForEntity(
                 url,
-                ValidationPipelineModel.class);
+                ValidationPipelineModel[].class);
 
         assertEquals(200, getResponse.getStatusCode().value());
-        var fetchedPipeline = getResponse.getBody();
-        assertNull(fetchedPipeline);
+        assertNotNull(getResponse.getBody());
+        var fetchedPipelines = getResponse.getBody();
+        assertEquals(0, fetchedPipelines.length);
     }
 
     @Test
