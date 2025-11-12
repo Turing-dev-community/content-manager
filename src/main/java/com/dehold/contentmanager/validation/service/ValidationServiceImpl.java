e5da8c073d4662649d0c667c9ae47ebada0425cb
Denis Holdeew <denis.h@turing.com>
2025-11-12 22:20:28 +0100

    Remove unused imports

--- src/main/java/com/dehold/contentmanager/validation/service/ValidationServiceImpl.java
+++ src/main/java/com/dehold/contentmanager/validation/service/ValidationServiceImpl.java
@@ -1,17 +1,13 @@
 package com.dehold.contentmanager.validation.service;
 
-import com.dehold.contentmanager.content.Content;
 import com.dehold.contentmanager.content.blogpost.model.BlogPost;
 import com.dehold.contentmanager.content.blogpost.service.BlogPostService;
-import com.dehold.contentmanager.validation.model.ContentTypeRegistry;
 import com.dehold.contentmanager.validation.model.ValidationStepType;
 import com.dehold.contentmanager.validation.pipeline.ValidationPipeline;
 import com.dehold.contentmanager.validation.pipeline.ValidationPipelineBuilder;
 import com.dehold.contentmanager.validation.model.ValidationResult;
 import com.dehold.contentmanager.validation.pipeline.ValidationPipelineFactory;
-import com.dehold.contentmanager.validation.pipeline.ValidationPipelineImpl;
 import com.dehold.contentmanager.validation.repository.ValidationResultRepository;
-import com.dehold.contentmanager.validation.step.LengthValidator;
 import com.dehold.contentmanager.validation.step.ValidationStep;
 import com.dehold.contentmanager.validation.step.ValidationStepFactory;
 import com.dehold.contentmanager.validation.web.dto.BlogPostValidationRequest;
