87badcbc54a6ddce153610a4c047bce10fc842ac
Denis Holdeew <denis.h@turing.com>
2025-11-14 10:21:04 +0100

    Add GenericConentModel

---
+++ src/main/java/com/dehold/contentmanager/content/generic/model/GenericContentModel.java
@@ -0,0 +1,83 @@
+package com.dehold.contentmanager.content.generic.model;
+
+import java.time.Instant;
+import java.util.Map;
+import java.util.UUID;
+
+public class GenericContentModel {
+    private UUID id;
+    private UUID userId;
+    private String type;
+    private Map<String, ContentFieldValue> fieldNameToValue;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private UUID parentId;
+
+    public GenericContentModel(UUID id, UUID userId, String type,
+                               Map<String, ContentFieldValue> fieldNameToValue,
+                               Instant createdAt, Instant updatedAt, UUID parentId) {
+        this.id = id;
+        this.userId = userId;
+        this.type = type;
+        this.fieldNameToValue = fieldNameToValue;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.parentId = parentId;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Map<String, ContentFieldValue> getFieldNameToValue() {
+        return fieldNameToValue;
+    }
+
+    public void setFieldNameToValue(Map<String, ContentFieldValue> fieldNameToValue) {
+        this.fieldNameToValue = fieldNameToValue;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(UUID parentId) {
+        this.parentId = parentId;
+    }
+}
