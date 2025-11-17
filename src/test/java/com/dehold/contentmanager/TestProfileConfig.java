75627bccdbb75f3f4c89e09415d43381fed6baf0
Denis Holdeew <denis.h@turing.com>
2025-11-17 13:57:52 +0100

    Proper test profile loading + security config
    Load test profile via application.yml. Also provide specific test security config

--- src/test/java/com/dehold/contentmanager/TestProfileConfig.java
+++ src/test/java/com/dehold/contentmanager/TestProfileConfig.java
@@ -2,21 +2,16 @@ package com.dehold.contentmanager;
 
 import org.springframework.boot.test.context.TestConfiguration;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @TestConfiguration
+@Profile("test")
 public class TestProfileConfig {
 
-    @PostConstruct
-    public void setTestProfile() {
-        System.setProperty("spring.profiles.active", "h2");
-    }
-
-    @Bean
+ @Bean
     public SecurityFilterChain testSecurityFilterChain(HttpSecurity http) throws Exception {
 
         http
