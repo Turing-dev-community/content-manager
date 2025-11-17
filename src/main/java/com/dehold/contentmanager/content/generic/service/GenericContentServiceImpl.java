21b8a53c4049ab49bc0e9d8a1d0acb122212f318
Denis Holdeew <denis.h@turing.com>
2025-11-17 09:07:41 +0100

    WIP Service impl

--- src/main/java/com/dehold/contentmanager/content/generic/service/GenericContentServiceImpl.java
+++ src/main/java/com/dehold/contentmanager/content/generic/service/GenericContentServiceImpl.java
@@ -1,4 +1,42 @@
 package com.dehold.contentmanager.content.generic.service;
 
-public class GenericContentServiceImpl {
+import com.dehold.contentmanager.content.generic.model.GenericContentModel;
+import com.dehold.contentmanager.content.generic.repository.GenericModelRepository;
+import com.dehold.contentmanager.exception.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class GenericContentServiceImpl implements GenericContentService {
+
+    private final GenericModelRepository repository;
+
+    public GenericContentServiceImpl(GenericModelRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public GenericContentModel getById(UUID id) {
+        return repository.findById(id).orElseThrow(() -> EntityNotFoundException.of("GenericContent", id.toString()));
+    }
+
+    @Override
+    public GenericContentModel save(GenericContentModel content) {
+        repository.save(content);
+        return repository.findById(content.getId()).orElseThrow(() -> EntityNotFoundException.of("GenericContent", content.getId().toString()));
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        if (!repository.existsById(id)) {
+            throw EntityNotFoundException.of("GenericContent", id.toString());
+        }
+        repository.deleteById(id);
+    }
+
+    @Override
+    public boolean existsById(UUID id) {
+        return repository.existsById(id);
+    }
 }
