9cff827f1bb23efa225b4ec9a03685126127eaa2
Denis Holdeew <denis.h@turing.com>
2025-11-20 14:49:04 +0100

    Simpify ContentTypeRegistry

--- src/main/java/com/dehold/contentmanager/validation/model/ContentTypeRegistry.java
+++ src/main/java/com/dehold/contentmanager/validation/model/ContentTypeRegistry.java
@@ -6,7 +6,6 @@ import com.dehold.contentmanager.content.customersupport.model.SupportRequest;
 import com.dehold.contentmanager.content.customersupport.model.SupportResponse;
 import com.dehold.contentmanager.content.generic.model.GenericContentModel;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class ContentTypeRegistry {
@@ -16,26 +15,8 @@ public class ContentTypeRegistry {
             "supportrequest", SupportRequest.class
     );
 
-    private static final Map<String, Class<? extends Content>> dynamicTypes = new HashMap<>();
-
     public static Class<? extends Content> getContentClass(String contentType) {
         String normalizedType = contentType.toLowerCase();
-
-        if (FIXED_TYPES.containsKey(normalizedType)) {
-            return FIXED_TYPES.get(normalizedType);
-        }
-
-        if (dynamicTypes.containsKey(normalizedType)) {
-            return dynamicTypes.get(normalizedType);
-        }
-
-        dynamicTypes.put(normalizedType, GenericContentModel.class);
-        return GenericContentModel.class;
-    }
-
-    public static Map<String, Class<? extends Content>> getAllContentTypes() {
-        Map<String, Class<? extends Content>> all = new HashMap<>(FIXED_TYPES);
-        all.putAll(dynamicTypes);
-        return all;
+        return FIXED_TYPES.getOrDefault(normalizedType, GenericContentModel.class);
     }
 }
